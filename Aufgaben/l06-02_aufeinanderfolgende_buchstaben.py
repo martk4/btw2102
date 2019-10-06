759fbe5ba6fab8240da1d40b177a6b856d195ce4
Marth Köbu Immanuel <koebuimmanuel.marth@students.bfh.ch>
2019-10-06 15:05:51 +0200

    done

--- Aufgaben/l06-02_aufeinanderfolgende_buchstaben.py
+++ Aufgaben/l06-02_aufeinanderfolgende_buchstaben.py
@@ -1,20 +1,25 @@
 the_input = "GZOOEHHCTRJYXXVUECLTFFFNKBHYHXXBYYYUUEYYGGLOHHGAAAASCWARRCHWYXXLBIZAAAXXLIWWWWWDDDDDRRHMMPPUUUHZNKKTXACCBJEYUIKHZZDPAKEMRCGCFFHHIIIFDDUUUGFFHR"
-
+#counter
 count= 0
+#lister
 doubles = []
 
-
+#read out string as a list
 for i in range(len(list(the_input))):
+#add to doubles list
     doubles.append(the_input[i])
 
 print("\n \n \n")
-
+# for every i in the list from the first until the last character
 for i in range(1,len(doubles)):
+     # if i in doubles is exactly the same as i-1 (character before)
        if doubles[i-1]==doubles[i]:
+            #add 2 to the count
             count+=2
        elif doubles[i-1]==doubles[i] == doubles[i+1]:
             count+=3
 
+#spit it
 print(count)
 
 #Indizierung
