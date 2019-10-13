90324ba9b37efeb78d5ea73ef4ab0679df157ee9
Marth Köbu Immanuel <koebuimmanuel.marth@students.bfh.ch>
2019-10-13 13:44:13 +0200

    in progress

--- mini_project_2/main.py
+++ mini_project_2/main.py
@@ -1,12 +1,13 @@
 ###############################################################
-#               SET UP
+#               SET UP                                        #
 ###############################################################
 
 import time
 import sys
+import random
 
 ###############################################################
-#               Design
+#               Design                                        #
 ###############################################################
 
 class colors:
@@ -44,7 +45,7 @@ class colors:
         lightgrey='\033[47m'
 
 ###############################################################
-#               INTRO
+#               INTRO                                         #
 ###############################################################
 
 def header():
@@ -58,7 +59,6 @@ def header():
     print()
     time.sleep(1.5)
     print("\n\n")
-
 def loader():
     print("LOADING")
     print()
@@ -68,7 +68,6 @@ def loader():
         sys.stdout.flush()
         time.sleep(0.05)
     print()
-
 def rules():
 
     print(colors.fg.green,"\n\tTHE RULES OF THE GAME",colors.reset)
@@ -86,68 +85,79 @@ def rules():
     print("Enter", end=" ")
     print(colors.bg.green,"n",colors.reset, end=" ")
     print("to start a new game. ")
-
-header()
-loader()
-rules()
-
-###############################################################
-#               GAME 
-###############################################################
-
-def game_loop():
-    def winner():
-        print("YOU WON")
-        print("How about another game?")
-
-    def loser():
-        print("YOU LOST")
-        print("How about another game?")
-    
-    heap_count= 20
-
+def quitter():
+    print ("See you soon. Goodbye!")
+    exit()
+def newer():
+    global heap_count
+    print("Starting a new game", end = "    ")
+    text= ". . ."
+    for c in text:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.2)
+    heap_count = 20
+    print("\n\n\n")
+    counter()
+def menu_choice():
     while True:
         key = input("Your input:")
         if key == "q":
-            print ("See you soon. Goodbye!")
-            break
-
+            quitter()
         elif key == "n":
-            print("Starting a new game.")
-            while True:
-                print(f"There are {heap_count} stones on the heap.\nHow many would like to draw? 1 or 2?\n")
-                key = input("Your input:")
-                if key == "q":
-                    break
-                if heap_count == 1:
-                    print("You lost")
-                    break
-                elif key == "1":
-                    heap_count -=1
-                    print(f"You drew 1 Stone.")
-                    if heap_count == 1:
-                        print("You won")
-                        break
-                elif key == "2":
-                    heap_count -=2
-                    print(f"You drew 2 Stone.")
-                    if heap_count == 1:
-                        print("You won")
-                        break
-                else:
-                    print("Sorry, I don't understand your input.\nPlease try again.")
-        
-        
-        print ("See you soon. Goodbye!")
-        break
-    
-    else:
-        print("Sorry, I don't understand your input.\nPlease try again.")
-        
-game_loop()
+            newer()
+        else:
+            print("Sorry, I don't understand your input.\nPlease try again.")
+
 
 ###############################################################
-#               AI PLAYER 
+#               GAME                                          #
+###############################################################
+heap_count= 20
+
+def winner():
+    print(colors.bg.green,"YOU WON",colors.reset)
+    exit()
+
+    exit()
+def loser():
+    print(colors.bg.red,"YOU LOST",colors.reset)
+    exit()
+def counter():
+    global heap_count
+    if heap_count == 20:
+        print(f"There is a heap of {heap_count} stones.")
+    elif heap_count <= 19:
+        print(f"There are {heap_count} stones on the heap.")
+    print("Would you like to remove 1 or 2 stones?")
+    print()
+    game()
+def game():
+    global heap_count
+    while True:
+        key = input("Your input:")
+        if key == "q":
+            quitter()        
+        elif key == "1":
+            heap_count -= 1
+            print(f"There are now {heap_count} stones on the heap.")
+            break
+        elif key == "2":
+            heap_count -= 2
+            print(f"There are now {heap_count} stones on the heap.")
+            break
+        elif key == "n":
+            newer()
+        #if heap_count == 1:
+            #winner()
+        else:
+            print("Sorry, I don't understand your input.\nPlease try again.")
+    if heap_count <= 1:
+        winner()
+    ai_player()
+        
+###############################################################
+#               AI PLAYER                                     #
 ###############################################################
 
 def choose_player():
@@ -155,3 +165,37 @@ def choose_player():
     print(colors.fg.yellow, "\t1 : Goofy the Goof",colors.reset)
     print(colors.fg.purple, "\t2 : Sherlock Holmes",colors.reset)
 
+def ai_player():
+    global heap_count 
+    print()
+    print("AI calculates next move", end = "    ")
+    text= ". . ."
+    for c in text:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.2)
+    print("\n")
+    a_choice= [1,2]
+    a= random.choice(a_choice)
+    
+    if a == 1 :
+        print(f"AI drew 1 stone.")
+        heap_count -= 1
+    if a == 2:
+        print(f"AI drew 2 stones.")
+        heap_count -=2
+    print()
+    if heap_count <= 1:
+        loser()
+    counter()
+
+
+###############################################################
+#               RUN IT                                        #
+###############################################################
+
+
+header()
+loader()
+rules()
+menu_choice()
