fb97f77fc09127b9fd1b01eb88e294585bbe9457
Marth Köbu Immanuel <koebuimmanuel.marth@students.bfh.ch>
2019-11-10 12:06:01 +0100

    done

--- mini_project_3/main.py
+++ mini_project_3/main.py
@@ -3,7 +3,9 @@ from os import listdir, path
 customers = []
 datalist = []
 final_dic = []
-
+cleanest = []
+header_theoutput = ""
+header_new = "Round 1"
 #   Readers        #
 def money(name_dir):
     f = open("mini_project_3/data/"+name_dir+'/money', 'r')
@@ -20,6 +22,11 @@ def customer_name(name_dir):
     name_cust = h.read()
     h.close()
     return name_cust
+def full_file():
+    f = open('mini_project_3/data/output.txt', 'r')
+    reader_full = f.read()
+    f.close()
+    return reader_full
 #   Data Finders   #
 def directories():
     master_dir = listdir("mini_project_3")
@@ -34,13 +41,14 @@ def customer_find(sub_dir):
             customers.append(sub_dir[a])
     return customers
 def data_find(customer_list):
+    
     for files in range(len(customer_list)):
         datalist.append(listdir("mini_project_3/data/"+customer_list[files]))
         for b in range(len(datalist)):
             temporary_store= {}
             for filename in range(len(datalist[0])):
                 if datalist[b][filename]== 'money':    
-                    temporary_store['money']= money(customers[b])
+                    temporary_store['money']= int(money(customers[b]))
                 elif datalist [b][filename] == 'name':
                    temporary_store['name']= customer_name(customers[b])
                 elif datalist [b][filename] == 'account':
@@ -50,6 +58,39 @@ def data_find(customer_list):
             else: 
                 final_dic.append(temporary_store)
     return final_dic
+def file_splitter(a_file):
+    #   Working Lists
+    clean = []
+    cleaner = []
+    
+    global header_theoutput
+    #   Splitting File
+    reading = a_file.split("*")
+    for elm in range(len(reading)):
+        if reading[elm] != "" and reading[elm] !="\n":
+            clean.append(reading[elm])
+    header = clean[0]
+    header_theoutput = header.replace("\n","")
+    clean.pop(0)
+    #   Splitting List
+    for i in range(0, len(clean), 3):
+        cleaner.append(clean[i:i + 3])
+    #   Pollish 
+    for q in range(len(cleaner)):
+        abc ="ABCDEFGHIJKLMNOPQRSTUVWXYZÖÄÜÈÉÀabcdefghijklmnopqrstuvwxyzöäüàéèç.,-"
+        temporary_dicti = {}
+        the_name = ""
+        temporary_dicti['account']=cleaner[q][0].replace(" ","")
+        for letter in range(len(cleaner[q][1])):
+            if cleaner[q][1][letter] in abc:
+                the_name += cleaner[q][1][letter]
+            elif cleaner[q][1][letter] == " " and cleaner[q][1][letter-1] in abc:
+                the_name += cleaner[q][1][letter]
+        temporary_dicti['name']=the_name[:-1]
+        temporary_dicti['money']=int(cleaner[q][2].replace(" ",""))
+        cleanest.append(temporary_dicti)
+        
+    return cleanest
 #   Table Design   #
 def name_sorter(to_sort):
     sorted_byname = sorted(to_sort, key=lambda k: k['name']) 
@@ -60,7 +101,7 @@ def table(data_stem):
     longest_account = 0
     longest_name = 0
     longest_money = 0
-
+     
     
 
     #   Length Finder
@@ -69,8 +110,8 @@ def table(data_stem):
             longest_account = len(l["account"])
         if len(l["name"]) > longest_name:
             longest_name= len(l["name"])
-        if len(l["money"]) > longest_money:
-            longest_money = len(l["money"])
+        if len(str(l["money"])) > longest_money:
+            longest_money = len(str(l["money"]))
 
     #   Table Builder
     for d in range(len(data_stem)):
@@ -82,7 +123,7 @@ def table(data_stem):
         padding_money= ""
         diff_account = int(longest_account - len(data_stem[d]["account"]))
         diff_name = int(longest_name - len(data_stem[d]["name"]))
-        diff_money = int(longest_money - len(data_stem[d]["money"]))
+        diff_money = int(longest_money - len(str(data_stem[d]["money"])))
 
     #   Space Adder
         for p in range(diff_account):
@@ -96,42 +137,99 @@ def table(data_stem):
 
         column_account = symbol+padding_base +data_stem[d]["account"]+padding_account+ padding_base
         column_name = symbol + padding_base +data_stem[d]["name"]+padding_name+ padding_base
-        column_money = symbol+padding_base +data_stem[d]["money"]+padding_money+ padding_base+symbol
+        column_money = symbol+padding_base +str(data_stem[d]["money"])+padding_money+ padding_base+symbol
         table +=  column_account + column_name + column_money+ "\n"
         total_lenght = int(len(column_account + column_name + column_money))
     return [table,total_lenght]
-#   Transfers      #
+#   Transactions      #
+def change_header():
+    global header_new 
+    read_header = full_file()
+    read_header = read_header.split("\n")
+    read_header = read_header[0]
+    if read_header == "Read from files:":
+        read_header = 0
+    else:
+        read_header = read_header.split(" ")
+        read_header = int(read_header[1])
+        header_new = "Round "+str(read_header+1)
 
+    return read_header
+def transactions_read(): 
+    global header_theoutput
+    global header_new
+    rounds= change_header()
+    fol = directories() 
+    transaction_dic = []
+    for j in fol:
+        if "transactions" in fol:
+            f = open('mini_project_3/data/transactions', 'r')
+            transacts = f.read()
+            f.close()
+        elif "transactions" not in fol:
+            print("Sorry, there seems to be no transactions today")
+    transaction_list = transacts.split("\n")
+
+    for full_info in transaction_list:
+        temp_dic = {}
+        temp_trans = full_info.split(",")
+        temp_dic['sender'] = temp_trans[0]
+        temp_dic['reciever'] = temp_trans[1]
+        temp_dic['amount'] = temp_trans[2]
+        transaction_dic.append(temp_dic)
+    return transaction_dic[rounds]
+def transactions_book():
+    case = transactions_read()
+    for tutto in range(len(cleanest)):
+        if case["sender"] == cleanest[tutto]["account"]:
+            cleanest[tutto]['money'] -= int(case["amount"])
+        if case["reciever"] == cleanest[tutto]["account"]:
+            cleanest[tutto]['money'] += int(case["amount"])
+    return case
 #   Output         #
-def output_file(content):
-    
-
+def output_file_first(content):
     #   Heading
     header = "Read from files:\n"
-    
     #   Liner
     liner_symbol = ""
     for t in range(content[1]):
         liner_symbol += "*"
     liner_finish= liner_symbol +"\n"
-
     #   Writing
-    if path.isfile('mini_project_3/data/output.txt') is False:
-        f = open('mini_project_3/data/output.txt', 'w')
-        f.write(header+liner_finish+content[0]+liner_finish)
-        f.close()
-        f = open('mini_project_3/data/output.txt', 'r')
-        reader = f.read()
-        f.close()
-    else:
-        f = open('mini_project_3/data/output.txt', 'r')
-        reader = f.read()
-        f.close()
-    
+    f = open('mini_project_3/data/output.txt', 'w')
+    f.write(header+liner_finish+content[0]+liner_finish)
+    f.close()
+    f = open('mini_project_3/data/output.txt', 'r')
+    reader = f.read()
+    f.close()
+    return reader
+def output_file_following(content):
+    global header_new
+    #   Heading
+    header = header_new
+    #   Liner
+    liner_symbol = ""
+    for t in range(content[1]):
+        liner_symbol += "*"
+    liner_finish= liner_symbol +"\n"
+    #   Writing
+    f = open('mini_project_3/data/output.txt', 'w')
+    f.write(header+"\n"+liner_finish+content[0]+liner_finish)
+    f.close()
+    f = open('mini_project_3/data/output.txt', 'r')
+    reader = f.read()
+    f.close()
     return reader
 #   Run This       #
 def run_it():
-    return output_file(table(name_sorter(data_find(customer_find(directories())))))
-
+    if path.isfile('mini_project_3/data/output.txt') is False:
+        final_output = output_file_first(table(name_sorter(data_find(customer_find(directories())))))
+    else:
+        file_splitter(full_file())
+        try:
+            transactions_book()
+            final_output= output_file_following(table(cleanest))
+        except IndexError:
+            final_output = "Sorry, there seems to be no more transactions"
+    return final_output
 print(run_it())
-
